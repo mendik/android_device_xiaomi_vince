2e9e2441a30ad63b379f594ac01ee9a990e5fd28
Gustavo Mend's <gusttavo.me@outlook.com>
2020-10-12 12:23:48 +0000

    Remove Target Gapps and add Havoc dependencies

--- havoc_vince.mk
+++ havoc_vince.mk
@@ -22,15 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common havoc stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-# PixelExperience Stuffs
-TARGET_GAPPS_ARCH := arm64
+# Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := aosp_vince
+PRODUCT_NAME := havoc_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
